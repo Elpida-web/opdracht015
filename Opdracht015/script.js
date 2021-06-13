684b55ecc62f192e5b1c59a88830d4077810a10f
Elpida-web <84407349+Elpida-web@users.noreply.github.com>
2021-06-13 12:33:15 +0200

    Add files via upload

---
+++ Opdracht015/script.js
@@ -0,0 +1,22 @@
+const makePizza = function () {
+    console.log("make dough");
+    console.log("make pizza bodem");
+    console.log("spread vegatables on bodem");
+    console.log("put some meat or fish on it");
+    console.log("put pizza in oven");
+};
+
+//* Sushi
+
+const makeSushi = function () {
+console.log("Use seaweed");
+console.log("Put some rice on it");
+console.log("Put some cumcumber on it");
+console.log("Roll it up");
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
